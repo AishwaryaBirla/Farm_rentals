c276d723b898fbf6f8b22c026992e48fc24552b3
AishwaryaBirla <53036180+AishwaryaBirla@users.noreply.github.com>
2021-11-20 23:55:20 +0530

    login

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,21 +1,112 @@
-const router = require('express').Router();
-let Login = require('../models/login.model');
+/** @format */
+
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const axios= require('axios');
+let FarmerRegistration = require("../models/farmersSignup.models");
+let LessorRegistration = require("../models/lessorSignup.models");
 //const { default: login } = require('../../src/Components/Login/Login');
 
-// router.route('/').get((req, res) => {
-//   User.find()
-//     .then(users => res.json(users))
-//     .catch(err => res.status(400).json('Error: ' + err));
-// });
-
-router.route('/login').post((req, res) => {
-  const fullname = req.body.fullname;
-  const newLogin = new Login({fullname});
-  console.log(newLogin+'ok');
-  newLogin.save()
-    .then(() => res.json('User added!'))
-    .catch(err => console.log(err));
-  console.log(Login.find().fullname);
+router.route("/login").post((req, res) => {
+  //const user= req.body.user;
+  //const password=req.body.password;
+  const rt = req.body.registrationType;
+  if (rt === "Farmer") {
+    //userdb= require('../models/farmersSignup.models');
+    //let usermodel= FarmerRegistration;
+    FarmerRegistration.findOne({ email: req.body.user }, function (err, docs) {
+      if (err) {
+        console.log(err);
+        FarmerRegistration.findOne(
+          { phone: req.body.user },
+          function (err, docs) {
+            if (err) {
+              console.log(err);
+            } else {
+              bcrypt.compare(
+                req.body.password,
+                docs.password,
+                function (err, result)
+                 {
+                  if (result === true) {
+                    //axios.get('http://localhost:3001/');
+                    console.log(result);
+                  }
+                  else{
+                    console.log(err);
+                  }
+                }
+              );
+            }
+          }
+        );
+      } else {
+        
+        bcrypt.compare(
+          req.body.password,
+          docs.password,
+          function (err, result) {
+            if (result === true) {
+              //console.log('ok');
+              // axios.get('http://localhost:3001/');
+              console.log(result);
+            }
+            else{
+              console.log(docs.password);
+              console.log(err);
+            }
+          }
+        );
+      }
+      //res.json({valid:sucess});
+    });
+  } else {
+    //userdb=require('../models/lessorSignup.models');
+    //let usermodel= LessorRegistration;
+    LessorRegistration.findOne({ email: req.body.user }, function (err, docs) {
+      if (err) {
+        console.log(err);
+        LessorRegistration.findOne(
+          { phone: req.body.user },
+          function (err, docs) {
+            if (err) {
+              console.log(err);
+            } else {
+              bcrypt.compare(
+                req.body.password,
+                docs.password,
+                function (err, result) {
+                  if (result === true) {
+                    // axios.get('http://localhost:3001/');
+                    console.log(result);
+                  }
+                  else{
+                    console.log(err);
+                  }
+                }
+              );
+            }
+          }
+        );
+      } 
+      else {
+        bcrypt.compare(
+          req.body.password,
+          docs.password,
+          function (err, result) {
+            if (result === true) {
+              //axios.get('http://localhost:3001/');
+              console.log(result);
+        }
+        else{
+          console.log(err);
+        }
+            
+          }
+        );
+      }
+    });
+  }
 });
 
 module.exports = router;
