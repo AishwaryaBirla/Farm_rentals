f9da8c4b0eab5bd000bf9c02b316df6db49e9a90
Veena Chaddha <59028294+veena-02@users.noreply.github.com>
2021-11-20 14:38:53 +0530

    Update ContactUs.js

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {FormGroup, Label, Input, Form, FormText, Button} from 'reactstrap'
 import './ContactUs.css'
+import MyNavbar from './../MyNavbar/MyNavbar';
 
 const ContactUs=()=>{
     return(
         <>
+           <MyNavbar />
             <div className="contact_background">
                 <Form className="contact_us">
                 <h3 className="text-center">Contact Us</h3>
