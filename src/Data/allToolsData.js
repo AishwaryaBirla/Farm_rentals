c396d281521b6beb55f10ff397a1ce865757a20c
Veena Chaddha <59028294+veena-02@users.noreply.github.com>
2021-11-20 14:42:34 +0530

    Add files via upload

---
+++ src/Data/allToolsData.js
@@ -0,0 +1,10 @@
+const allTools = {
+    tractor: ["mini tractor","4wd tractor","ac cabin tractor"],
+    implements : ["rotary tiller","cultivator", "plough", "harrow", "trailer","sprayer"],
+    harvester : ["self propelled", "tractor-mounted"],
+    tools : ["tarpaulin","sprayers","brush cutter","power weeder","power tiller","power reaper","earth auger","mowers and trimmers","accessories"]
+};
+const getAllTools = () =>{
+    return allTools;
+}
+export default getAllTools;
